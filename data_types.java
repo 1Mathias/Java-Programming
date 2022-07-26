1d169a98d88db85252750ac1f44fcfd3d8990519
Mathias <mathias@Mathiass-MacBook-Pro.local>
2022-07-26 12:59:47 +0300

    Data Types

---
+++ data_types.java
@@ -0,0 +1,11 @@
+
+public class data_types {
+   public  static void main(String[] args){
+       int myNumber=20;
+       float myFloat=5.8f; //Floating
+       char myLetter='M'; //Character
+       boolean myBool=true; //Boolean
+       String myText="Hello"; //String
+
+   }
+}
