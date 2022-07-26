7e23484111874cd12f8103ba80bcc583f2f78adc
Mathias <mathias@Mathiass-MacBook-Pro.local>
2022-07-26 12:33:56 +0300

    First Application

---
+++ firstapp.java
@@ -0,0 +1,5 @@
+public class firstapp {
+    public static void main(String[] args){
+        System.out.println("Hello I am Mathias Abdissa, This is a greeting from my first Java test Application Thankyou Amigos");
+    }
+}
