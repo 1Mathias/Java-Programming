b58e5a7861f4d9f5e7370aba183194b696e8f8c8
Mathias <mathias@Mathiass-MacBook-Pro.local>
2022-07-26 12:14:22 +0300

    Array Length

---
+++ arraylength.java
@@ -0,0 +1,6 @@
+public class arraylength {
+    public static void main(String[] args){
+        String[] cars={"Mazda","Mercedes","Volvo","BMW"};
+        System.out.println("Array Length " + cars.length);
+    }
+}
