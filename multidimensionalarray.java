2bfda750cbba1ed7cf6b41c2ef95793942dedfab
Mathias <mathias@Mathiass-MacBook-Pro.local>
2022-07-26 12:34:26 +0300

    Multidimensional Array

--- multidimensionalarray.java
+++ multidimensionalarray.java
@@ -1,5 +1,20 @@
 public class multidimensionalarray {
     public static void main(String[] args){
+        int[][] mynumbers={{1,2,3,4},{5,6,7}};
+        int x=mynumbers[0][2];
+        System.out.println(x);
+        System.out.println(mynumbers.length);
+        for(int i=0;i<mynumbers.length;i++){
+            for(int j=0;j<mynumbers[i].length;j++){
+                if(i==0){
+                    System.out.println("First Array "+mynumbers[i][j]);
+                }
+                else{
+                    System.out.println("Second Array "+mynumbers[i][j]);
+                }
+                System.out.println(mynumbers[i][j]);
 
+            }
+        }
     }
 }
