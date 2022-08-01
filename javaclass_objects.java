3366df915058f07c47909ccbf795a43b85dcd841
Mathias <mathias@Mathiass-MacBook-Pro.local>
2022-08-01 10:46:20 +0300

    Java classes and objects

--- javaclass_objects.java
+++ javaclass_objects.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class javaclass_objects {
+// A Class is like an object constructor,
+// or a "blueprint" for creating objects
+
+public class javaclass_objects {
+    int x = 5;
+
+    public static void main(String[] args) {
+        javaclass_objects myobj1 = new javaclass_objects();
+        javaclass_objects myobj2 = new javaclass_objects();
+        System.out.println(myobj1.x);
+        System.out.println(myobj2.x);
+
+        myobj1.myMethod();
+        myobj1.speed(200);
+    }
+
+    static void myMethod() {
+        System.out.println("It works");
+    }
+
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is " + maxSpeed);
+    }
 }
