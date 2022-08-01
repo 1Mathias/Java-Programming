a181626eef9ab6a3b6fd574f4c22e54efa33e59c
Mathias <mathias@Mathiass-MacBook-Pro.local>
2022-08-01 10:35:31 +0300

    Git Ignore Added

---
+++ constructor.java
@@ -0,0 +1,15 @@
+public class constructor {
+    int x;
+    int modelYear;
+    String modelName;
+
+
+    public constructor(int year,String name){
+        modelName=name;
+        modelYear=year;
+    }
+    public static void main(String[] args){
+        constructor mycar=new constructor(1969,"Mustang");
+        System.out.println(mycar.modelYear+" "+ mycar.modelName);
+    }
+}
