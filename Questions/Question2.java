b45797e40472a710eabe0f72a69bfaaf181d76ea
1Mathias <mathiasabdisa@gmail.com>
2022-08-08 22:22:19 +0300

    Question two Added

--- Questions/Question2.java
+++ Questions/Question2.java
@@ -1,2 +1,26 @@
-package Questions;public class Question2 {
+package Questions;
+
+public class Question2 {
+    public static void main(String[] args) {
+        int[] a={11,4, 7};
+        System.out.println(isMagicArray(a));
+    }
+
+    static int isMagicArray(int[] a){
+        int sum=0;
+        if(a[0]==0)
+            return 1;
+        for(int i=0;i<a.length;i++){
+            int count=0;
+            for(int j=2;j<a[i];j++){
+                count++;
+            }
+            if(count==0&&a[i]>0)
+                sum+=a[i];
+        }
+        if(sum==a[0])
+            return 1;
+        return  0;
+    }
 }
+
