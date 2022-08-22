11b537c6c45864705a936b377d1596766362a242
1Mathias <mathiasabdisa@gmail.com>
2022-08-22 13:30:06 +0300

    question four and loop updates

--- Questions/Question4.java
+++ Questions/Question4.java
@@ -1,2 +1,25 @@
-package Questions;public class Question4 {
+package Questions;
+
+public class Question4 {
+    public static void main(String[] args) {
+
+    }
+
+    static int isprimeproduct(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0 && isPrime(i)) {
+                for (int j = i + 1; j < n; j++)
+                    if (n % j == 0 && isPrime(j)) {
+                        if (i * j == n)
+                            return 1;
+                    }
+
+            }
+        }
+        return 0;
+    }
+
+    static int isPrime(int i) {
+        return 0;
+    }
 }
--- whileloop.java
+++ whileloop.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class whileloop {
+public class whileloop {
+    public static void main(String[] args){
+        int i=0;
+        while(i<10){
+            System.out.println("I is "+i);
+            i++;
+        }
+    }
 }
