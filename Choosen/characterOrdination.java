824f3725c4c5bff05037cab331dc379e028c2973
1Mathias <mathiasabdisa@gmail.com>
2022-09-05 23:21:27 +0300

    CharaceterOrdination Solution

---
+++ Choosen/characterOrdination.java
@@ -0,0 +1,18 @@
+package Choosen;
+
+public class characterOrdination {
+    public static void main(String[] args) {
+        System.out.println(characterOrdination(new char[]{'a','b','c'},1,2));
+    }
+
+    static char[] characterOrdination(char[] a, int start, int length) {
+        if (length < 0 || start < 0 || start + length - 1 >= a.length) {
+            return null;
+        }
+        char[] sub=new char[length];
+        for(int i=start,j=0;j<length;i++,j++){
+            sub[j]=a[i];
+        }
+        return sub;
+    }
+}
